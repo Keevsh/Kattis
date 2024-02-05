4d19fe23e7e0cb65ac07978acf766ef0507fa1c9
Kevsh <134569457+Keevsh@users.noreply.github.com>
2024-02-04 23:28:01 -0400

    Add files via upload

---
+++ C#/A Real Challenge.cs
@@ -0,0 +1,14 @@
+﻿namespace Kattis;
+
+using System;
+
+class ARealChallenge
+{
+    static void Main()
+    {
+        long Area = long.Parse(System.Console.ReadLine());
+        double Longitud = Math.Sqrt(Area);
+        double Perimetro = 4 * Longitud;
+        System.Console.WriteLine(Perimetro);
+    }
+}
